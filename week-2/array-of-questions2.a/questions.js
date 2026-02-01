d2b8ec505c2a4a76fc81d1d23713c54719feff75
Your Name <ckoutianov@mcad.edu>
2026-02-01 11:56:19 -0800

    Initial questions.js

---
+++ week-2/array-of-questions2.a/questions.js
@@ -0,0 +1,17 @@
+//Array of questions
+const myQuiz = [
+    {
+        question: "What do you use to color that comes in a box of 24",
+        answer: "Crayons"
+    },
+    {
+        question: "What do your call art made by folding paper, that comes from Japan?",
+        answer: "Origami"
+    },
+    {
+        question: "Wha do artist use to remove pencil marks from paper?",
+        answer: "An eraser"
+    }
+
+]
+console/log(myQuiz);
