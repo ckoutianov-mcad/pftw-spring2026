c668e5ff99f8b5ca0b7a6a757b8e16043694f176
Your Name <ckoutianov@mcad.edu>
2026-02-05 19:04:50 -0800

    pattern styled pattern.js

--- week-3/parameter-tile3.2ex/pattern.js
+++ week-3/parameter-tile3.2ex/pattern.js
@@ -4,33 +4,39 @@ function setup() {
 
 function createTile(originX, originY, primaryColor, secondaryColor, circleOutlineColor, dotColor) {
   translate(originX, originY);
+  //background color
   fill(primaryColor);
   rect(0, 0, 200, 200);
+  // x on tile
   stroke(secondaryColor);
-  strokeWeight(15);
+  strokeWeight(10);
   line(0, 0, 200, 200);
   line(200, 0, 0, 200);
-  stroke("white");
+  
+  //small circles
+  stroke("brown");
   strokeWeight(10);
   fill(dotColor);
   circle(100, 50, 50);
   circle(50, 100, 50);
   circle(150, 100, 50);
   circle(100, 150, 50);
-  fill("gray");
+   
+  //center circle
+  fill("silver");
   stroke(circleOutlineColor);
   strokeWeight(10);
   circle(100, 100, 100);
 }
 function draw() {
-  createTile(0, 0, 'red', 'white', 'green', 'magenta');
-  createTile(0, 200, 'green', 'yellow', 'blue', 'violet');
-  createTile(0, 200, 'blue', 'white', 'green', 'magenta');
-  createTile(200, -400, 'cyan', 'yellow', 'blue', 'violet');
-  createTile(0, 200, 'red', "white", "green", "magenta");
-  createTile(0, 200, 'green', "yellow", "blue", "violet");
-  createTile(200, -400, 'blue', "white", "green", "magenta");
-  createTile(0, 200, 'cyan', 'purple', 'green', 'gray');
-  createTile(0, 200, 'red', "purple", "green", "gray");
+  createTile(0, 0, 'teal', 'white', 'aqua', 'magenta');
+  createTile(0, 200, 'green', 'tomato', 'lime', 'violet');
+  createTile(0, 200, 'olive', 'white', 'aqua', 'magenta');
+  createTile(200, -400, 'cyan', 'tomato', 'lime', 'violet');
+  createTile(0, 200, 'teal', 'white', 'aqua', 'magenta');
+  createTile(0, 200, 'green', 'tomato', 'lime', 'violet');
+  createTile(200, -400, 'olive', 'white', 'aqua', 'magenta');
+  createTile(0, 200, 'cyan', 'tomato', 'lime', 'violet');
+  createTile(0, 200, 'teal', 'orange', 'aqua', 'magenta');
 
 }
