4c2a0ddbc306ac6daa5e3eebdcc780ec8ee46511
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-02-12 20:11:19 -0800

    user instructions added events.js

--- week-4/p5-events4.2ex/events.js
+++ week-4/p5-events4.2ex/events.js
@@ -1,27 +1,46 @@
 //animate var
 let rectX = 0;
+const rectWidth = 75;
+let rectY;
+const rectHeight= 75;
+
+
+//hit count
+let clickCount = 0;
+
+//speed
+let speed;
 
 //dimension of canvas
 function setup () {
     createCanvas(500, 500);
+    rectY = random(height - rectHeight);
+    speed = random(1, 3);
+    window.alert('Click on the square with your mouse as fast as possible! Your score will be recorded.')
 }
 
 //canvas for the shape
 function draw () {
     background(155);
     drawShape();
-    rectX += 2;
-    noLoop()
+    rectX+= speed;
+     if (rectX > width) {
+       noLoop();
+       text("Your score was " + clickCount, 250, 300);
+     }
 }
 
 //exact target hit
 function mousePressed () {
-    if ((mouseX >= rectX && mouseX <= 75) && (mouseY >=0 && mouseY<=75)) {console.log('Right on target!');
+    if ((mouseX >= rectX && mouseX <= rectX + rectWidth) && (mouseY >= rectY && mouseY<= rectY + rectHeight)) {
+    clickCount++;
+    console.log('Hit #', clickCount);
     }
+   
 }
 
 //style of shape
 function drawShape () {
-    fill('blue');
-    rect(rectX, 0 , 75, 75);
+    fill('black');
+    rect(rectX, rectY , rectWidth, rectHeight);
 }
