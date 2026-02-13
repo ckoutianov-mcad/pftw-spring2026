82d9a812f0d23b520f7ee0369f041a3b25bd479b
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-02-12 18:41:40 -0800

    update events.js

--- week-4/dom-events4.1ex/events.js
+++ week-4/dom-events4.1ex/events.js
@@ -1,9 +1,9 @@
 // select empty div and assign to a variable
-const myEmptyDiv = document.querySelector("#myEmptyDiv");
+const myEmptyDiv = document.querySelector('#myEmptyDiv');
 
 //placing h1 and create a new element
 const heading = document.createElement('h1');
-heading.innerHTML = 'Hello everyone! It is a rainy day!';
+heading.innerHTML = 'Hello everyone! It is a cloudy day. ☁️';
 
 //cursor mouse styled
 heading.style.cursor = 'pointer';
@@ -13,8 +13,7 @@ heading.addEventListener('click', handleHeadingClick);
 myEmptyDiv.appendChild(heading);
 
 //function
-function handleHeadingClick() {
-    console.log('Heading has been clicked');
+function handleHeadingClick () {
     document.body.style.backgroundColor = 'lightblue';
 }
 
