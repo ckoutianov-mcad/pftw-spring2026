d5b0e62f7eef22ccb326f76f9112f66e45ce0972
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-02-12 19:32:36 -0800

    update target hit for shape events.js

--- week-4/p5-events4.2ex/events.js
+++ week-4/p5-events4.2ex/events.js
@@ -1,14 +1,21 @@
+//dimension of canvas
 function setup () {
     createCanvas(500, 500);
-
 }
 
+//canvas for the shape
 function draw () {
     background(155);
     drawShape();
-
 }
 
+//exact target hit
+function mousePressed () {
+    if ((mouseX >= 0 && mouseX <= 75) && (mouseY >=0 && mouseY<=75)) {console.log('Right on target!');
+    }
+}
+
+//style of shape
 function drawShape () {
     fill('blue');
     rect(0, 0 , 75, 75);
