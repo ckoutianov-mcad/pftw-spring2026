02008ba35a40a41b48636fbb9f49da8733ab4b16
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-02-12 18:26:45 -0800

    initial click event events.js

---
+++ week-4/dom-events4.1ex/events.js
@@ -0,0 +1,17 @@
+// select empty div and assign to a variable
+const myEmptyDiv = document.querySelector("#myEmptyDiv");
+
+//placing h1 and create a new element
+const heading = document.createElement('h1');
+heading.innerHTML = 'Hello everyone! It is a rainy day!';
+
+heading.addEventListener('click', handleHeadingClick);
+//attach to DOM
+myEmptyDiv.appendChild(heading);
+
+//function
+function handleHeadingClick() {
+    console.log('Heading has been clicked');
+    document.body.style.backgroundColor = 'green';
+}
+
