131765030712fd9ec7a79dc18a814190c0cce6ac
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-02-12 19:24:02 -0800

    updated canvas events.js

--- week-4/p5-events4.2ex/events.js
+++ week-4/p5-events4.2ex/events.js
@@ -1,3 +1,15 @@
 function setup () {
-    
+    createCanvas(500, 500);
+
+}
+
+function draw () {
+    background(155);
+    drawShape();
+
+}
+
+function drawShape () {
+    fill('blue');
+    rect(0, 0 , 75, 75);
 }
