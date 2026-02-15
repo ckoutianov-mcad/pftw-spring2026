a520589359d9cfbb2e51a5056ad42d1e65d6f3a4
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-02-15 12:21:53 -0800

    event added grid.js

--- week-4/memory-game-4.a/grid.js
+++ week-4/memory-game-4.a/grid.js
@@ -2,12 +2,19 @@
 const circleDiameter = 100;
 let circleX = 200;
 let circleY = 300; 
-let myCircles=[];
+
 
 //canvas setup
 function setup () {
     createCanvas(1000, 500);
-    background("#617891");}
+    background("#617891");
+    ellipse(circleX, circleY, circleDiameter);
+}
 
-
-   
+//event
+function mousePressed () {
+    let distance = dist(mouseX, mouseY, circleX, circleY);
+    if (distance < circleDiameter / 2) {
+        console.log('circle clicked');
+    }
+}
