bf7f0713fb24fbee45c558d01367ebb496ed3c55
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-02-15 11:31:06 -0800

    initial grid.js

---
+++ week-4/memory-game-4.a/grid.js
@@ -0,0 +1,13 @@
+//var setup
+const circleDiameter = 100;
+let circleX = 200;
+let circleY = 300; 
+let myCircles=[];
+
+//canvas setup
+function setup () {
+    createCanvas(1000, 500);
+    background("#617891");}
+
+
+   
