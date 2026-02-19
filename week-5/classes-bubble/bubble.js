e7cdaceb8bf82a052d8daee339ebcb4fc4673f6e
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-02-18 22:14:20 -0800

    classes added bubble.js

--- week-5/classes-bubble/bubble.js
+++ week-5/classes-bubble/bubble.js
@@ -1,1 +1,18 @@
-function setup ()
+
+class Bubble {
+    constructor() {
+        this.x = 200;
+        this.y = 250;
+    }
+
+    move () {
+        
+    }
+
+    show() {
+        stroke(255);
+        strokeWeight(4);
+        fill("#815BFF");
+        ellipse(this.x, this.y, 24, 24)
+    }
+}
