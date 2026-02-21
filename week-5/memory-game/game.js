362d42fe022e27f16c44d175ca3c90db03c8a2eb
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-02-21 11:56:47 -0800

    array updated game.js

--- week-5/memory-game/game.js
+++ week-5/memory-game/game.js
@@ -1,11 +1,30 @@
 //var setup
 let myCard;
+const DOWN = 'down';
+const UP = 'up';
+let startingX = 100;//loops
+let startingY = 100;
+let cards = [];
+gameState = {
+
+}
 
 //canvas setup
 function setup() {
-  createCanvas(1000, 800);
-  background("#30364F"); 
-  myCard = new Card();
+  createCanvas(800, 600);
+  background("#30364F");
+  for (let j = 0; j < 2; j++) {
+    for (let i = 0; i < 5; i++) {
+    cards.push(new Card(startingX, startingY)); 
+    startingX += 150;
+    console.log(startingX);}
+  }
+    startingY += 150;
+    startingX = 100
+  {
+
+ startingX += 100;
+  }
   //alert('Match the fruit slices.')
 }//end setup
 
@@ -16,29 +35,41 @@ function mousePressed() {
 
 //class constructor
 class Card {
-    constructor() {
-    this.x = 180;
-    this.y = 180;
-    //this.diameter = 10;
+    constructor(x, y) {
+    this.x = x;
+    this.y = y;
     this.width = 120;
     this.height = 120;
+    this.face = DOWN;
     this.show();
 }//end card
 show () {
-    fill("#E1D9BC");
-    strokeWeight(5);
-    stroke("#F0F0DB");
-    ellipse(this.x, this.y, this.width, this.height);
-    //rect(this.x, this.y, this.width, this.height, 5);
+    if (this.face === DOWN)
+        {
+          fill("#E1D9BC");
+          ellipse(this.x, this.y, this.width, this.height);
+        } else {
+            fill("#ff7444");
+            ellipse(this.x, this.y, this.width, this.height);
+    }
 }//end show
 didHit (mouseX, mouseY) {
     if (mouseX >= this.x && mouseX <= this.x + this.width &&
         mouseY >= this.y && mouseY <= this.y + this.height) {
+            this.flip();
             return true;
         } else {
             return false;
         } 
     } //x,y
+flip () {
+    if (this.face === DOWN) {
+        this.face = UP;
+    } else {
+        this.face = DOWN;
+    }
+        this.show();
+}//end flip
 
 
 //end of card-constructor       
