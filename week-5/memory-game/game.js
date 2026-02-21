eaf10baa8c0ea5c8e29808f43bc4e41ad176ec52
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-02-21 10:30:07 -0800

    ellipse update game.js

--- week-5/memory-game/game.js
+++ week-5/memory-game/game.js
@@ -1,28 +1,51 @@
 //var setup
-
+let myCard;
 
 //canvas setup
 function setup() {
   createCanvas(1000, 800);
-  background("#617891"); 
-  let myCard = new Card();
+  background("#30364F"); 
+  myCard = new Card();
   //alert('Match the fruit slices.')
-}
+}//end setup
+
+//event
+function mousePressed() {
+    console.log(myCard.didHit(mouseX, mouseY));
+}//end event
 
 //class constructor
 class Card {
     constructor() {
-    this.x = 100;
-    this.y = 100;
-    this.width = 80;
-    this.height = 100;
+    this.x = 180;
+    this.y = 180;
+    //this.diameter = 10;
+    this.width = 120;
+    this.height = 120;
     this.show();
-}
-
+}//end card
 show () {
-    fill ("#000000");
-    rect(this.x, this.y, this.width, this.height, 5);
+    fill("#E1D9BC");
+    strokeWeight(5);
+    stroke("#F0F0DB");
+    ellipse(this.x, this.y, this.width, this.height);
+    //rect(this.x, this.y, this.width, this.height, 5);
+}//end show
+didHit (mouseX, mouseY) {
+    if (mouseX >= this.x && mouseX <= this.x + this.width &&
+        mouseY >= this.y && mouseY <= this.y + this.height) {
+            return true;
+        } else {
+            return false;
+        } 
+    } //x,y
+
+
+//end of card-constructor       
 }
 
-//end of card-constructor
-}
+
+
+
+
+
