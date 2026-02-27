ba978c154165e42e873ec44d4ab43c4f4f5e1650
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-02-26 20:56:21 -0800

    react exercise

---
+++ week-6/my-movies/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+
+function App() {
+const name = "Cinthia";  
+const movie = "Classic Horror"
+  return (
+  <div>Hello World, my name is {name}. My favorite type of movies are {movie}.</div>
+  )
+}
+
+export default App
