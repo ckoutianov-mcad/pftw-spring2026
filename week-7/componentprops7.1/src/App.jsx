88484b9090b09c80e0ca0e4a68990eac2709e9b9
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-03-05 19:14:21 -0800

    update Camelid.jsx

--- week-7/componentprops7.1/src/App.jsx
+++ week-7/componentprops7.1/src/App.jsx
@@ -1,16 +1,27 @@
+import Camelid from "./components/Camelid";
+ 
 import './App.css'
 
 function App() {
+const camelidData = [
+  {name: "Llama",
+  trivia: "Cute Llamas vs. Alpacas: Facts and a DebateLlamas are intelligent, social camelids from South America that can carry up to 200 pounds, reach speeds of 35 mph, and communicate by humming."
+  },
 
+  {name: "Alpaca",
+  trivia: "Alpacas are highly social, gentle camelids from South America, renowned for their luxurious, hypoallergenic fleece and unique humming communication."
+  }
+]
 
   return (
     <>
- 
       <div> 
-        <h2>LLAMAS</h2>
+        <h2>CAMELIDS</h2>
+        <Camelid 
+        name={camelidData[0].name}
+        />
       </div>
   
-     
     </>
   )
 }
---
+++ week-7/componentprops7.1/src/components/Camelid.jsx
@@ -0,0 +1,12 @@
+function Camelid({name}) {
+    console.log(name)
+    return (
+        <div>
+            <h1>{name}</h1>
+            <p></p>
+            <div></div>
+        </div>
+    );
+
+}
+ export default Camelid
