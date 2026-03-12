deb1e51491b4b40b78e904506164f3dbd9af7f65
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-03-12 13:43:11 -0700

    update styling App.css

--- week-8/animal-list-ex8.2/src/ButterflyComponent.jsx
+++ week-8/animal-list-ex8.2/src/ButterflyComponent.jsx
@@ -1,11 +1,12 @@
 export default function ButterflyComponent ({
     butterflyName, clickFocus, clickDelete
-}) {
+}) 
+{
     return (
         <div>
-            <div>{butterflyName}</div>
+            <div className="butterflyCard">{butterflyName}</div>
             <div>
-                <button onClick={() => {clickFocus(butterflyName);}}> Focus </button>
+                <button onClick={() => {clickFocus(butterflyName);}}> Focus </button> |  
                 <button onClick={() => {clickDelete(butterflyName);}}> Delete </button>
             </div>
         </div>
