1627c29d587a450d66b1764ba178edb9e28eca8a
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-03-12 19:44:53 -0700

    update App jsx

--- week-8/camelid-img8.1/src/App.jsx
+++ week-8/camelid-img8.1/src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import defaultImage from "./assets/alpacas-vs-llamas.jpg";
+import llamaImage from "./assets/llama-img.jpg";
+import alpacaImage from "./assets/alpaca-img.jpg";
 import './App.css'
 
 function App() {
-  const [currentImage, setCurrentImage] = useState('.jpg');
+  const [currentImage, setCurrentImage] = useState(defaultImage);
 
   return (
     <>
+  <h1>Choose your Camelid!</h1>
+  <div> <img src={currentImage} alt="Alpaca Vs Llama" /></div>
 
+  <div> <button onClick={() => setCurrentImage(llamaImage)}>Llama</button></div>
+  | 
+  <div> <button onClick={() => setCurrentImage(alpacaImage)}>Alpaca</button></div>
     </>
-  )
+  );
 }
 
 export default App
--- week-8/img-swap-ex8.1/src/App.jsx
+++ week-8/img-swap-ex8.1/src/App.jsx
@@ -2,13 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [llamaPic, setLlamaPic] = useState (0);
+  const [alpacaPic, setAlpacaPic] = useState (0);
   return (
     <>
-     
+      <h1> Choose your camelid!</h1>
+      <div>
+        Llama <button onClick={() => {
+          setLlamaPic(llamaPic +1);
+        }}>Llamas Rule!</button>
+      </div>
+      <div>
+        Alpaca <button onClick={() => {
+          setAlpacaPic(alpacaPic +1);
+        }}>Alpaca Rule!</button>
+      </div>
+      <div>
+        <h2>Camelid favorites</h2>
+        Llamas {llamaPic}  VS Alpacas {alpacaPic}
+
+      </div>
     </>
-  )
+  );
 }
 
 export default App
