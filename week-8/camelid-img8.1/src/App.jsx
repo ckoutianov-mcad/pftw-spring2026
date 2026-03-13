216f066f5f9c2faa2b8421392f3f62d1876c8371
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-03-12 20:31:17 -0700

    update App.css

--- week-8/camelid-img8.1/src/App.jsx
+++ week-8/camelid-img8.1/src/App.jsx
@@ -6,15 +6,14 @@ import './App.css'
 
 function App() {
   const [currentImage, setCurrentImage] = useState(defaultImage);
-
+  
   return (
     <>
   <h1>Choose your Camelid!</h1>
   <div> <img src={currentImage} alt="Alpaca Vs Llama" /></div>
-
-  <div> <button onClick={() => setCurrentImage(llamaImage)}>Llama</button></div>
-  | 
-  <div> <button onClick={() => setCurrentImage(alpacaImage)}>Alpaca</button></div>
+  <div> <button onClick={() => setCurrentImage(llamaImage)}>Llama</button>
+  🦙
+   <button onClick={() => setCurrentImage(alpacaImage)}>Alpaca</button></div>
     </>
   );
 }
