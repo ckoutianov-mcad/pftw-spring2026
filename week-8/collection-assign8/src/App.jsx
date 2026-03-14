adfe7f6eb8ca993ef372ca37ae9818c34769c69b
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-03-14 10:31:49 -0700

    public, App.jsx with array and images

--- week-8/collection-assign8/src/App.jsx
+++ week-8/collection-assign8/src/App.jsx
@@ -1,119 +1,68 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fragrances, setFragrances] = useState
+  ([
+    {
+      name: "Prada Paradoxe Eau de Parfum",
+      fragranceFamily: "Florals",
+      scentType: "Warm Florals",
+      notes: "Neroli Bud, White Amber, White Musk",
+      description:
+        "This is a floral, ambery perfume that reinvents freshness with a neroli bud extraction to capture the flower’s fresh scent, sensuality with bio-converted amber to reveal a vibrant warmth, and intensity through a revolutionary musk molecule for an intense yet subtle trail.",
+      isLayerable: false,
+      id: 1
+    },
+
+    {
+      name: "Kayali Yum Boujee Marshmallow Eau de Parfum",
+      fragranceFamily: "Warm & Spicy",
+      scentType: "Warm & Sweet Gourmands",
+      notes: "Strawberry, Pink Marshmallow, Whipped Vanilla",
+      description:
+        "TThis fragrance features a delightful menu of mouthwatering notes, including fluffy pink marshmallow, succulent strawberry, flaky coconut, delicate freesia, whipped vanilla, and pink musk. Boujee, fluffy, and delicious, this luxurious scent will linger with sweet temptation with every spritz.",
+      isLayerable: true,
+      id: 2
+    },
+
+    {
+      name: "Valentino Donna Born in Roma Eau de Parfum",
+      fragranceFamily: "Florals",
+      scentType: "Warm Florals",
+      notes: "Blackcurrant, Jasmine Grandiflorum, Bourbon Vanilla",
+      description:
+        "Inspired by Rome, a place where past and present coexist, Born in Roma fragrances tell a story of self-expression: a celebration of people living freely, while embracing their heritage.",
+      isLayerable: false,
+      id: 3
+    },
+
+    {
+      name: "Chanel Chance Eau Tendre Eau de Parfum",
+      fragranceFamily: "Florals",
+      scentType: "Fruity Florals",
+      notes: "Grapefruit, Quince, Rose Accord, White Musk",
+      description:
+        "An enhanced interpretation of the unexpected floral-fruity fragrance, CHANCE EAU TENDRE Eau de Parfum sweeps you into an intensified whirlwind of tenderness. The signature CHANCE bottle is reinvented with a silver cap and golden accent.",
+      isLayerable: false,
+      id: 4
+    },
+
+    {
+      name: "DedCool Mochi Milk Eau de Parfum",
+      fragranceFamily: "Warm & Spicy",
+      scentType: "Warm & Sweet Gourmands",
+      notes: "Marshmallow, Vanilla Bean, Sweet Rice Milk",
+      description:
+        "Mochi Milk is the newest extension of DedCool’s MILK family of skin scents—fragrances that smell like you, only cozier. Mochi Milk is a sweeter, gourmand take on the warm notes found in both Milk and Xtra Milk. Think sweet rice milk, marshmallows, and vanilla. Words that describe Mochi Milk are sweet, whimsical, and nostalgic.",
+      isLayerable: true,
+       id: 5
+    },
+  ]);
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      
     </>
   )
 }
