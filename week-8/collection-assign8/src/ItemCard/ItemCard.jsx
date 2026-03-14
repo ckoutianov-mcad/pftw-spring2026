d63bc17a968410f4e566add9374321c62a3c4c08
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-03-14 15:03:28 -0700

    update ItemCard.jsx

--- week-8/collection-assign8/src/ItemCard/ItemCard.jsx
+++ week-8/collection-assign8/src/ItemCard/ItemCard.jsx
@@ -1,5 +1,25 @@
-export default function ItemCard() {
+import PropTypes from "prop-types";
+import "./Itemcard.css";
+export default function ItemCard({
+    name,
+    fragranceFamily,
+    scentType,
+    notes,
+    description,
+    isLayerable,
+    image,
+    id
+}){
     return (
-        Item Card
-    )
-}
+        <div className="fragranceCard">hello</div>
+    )}
+    ItemCard.propTypes = {
+      name: PropTypes.string,
+      fragranceFamily: PropTypes.string,
+      scentType: PropTypes.string,
+      notes: PropTypes.string,
+      description: PropTypes.string,
+      isLayerable: PropTypes.bool,
+      image: PropTypes.string,
+      id: PropTypes.string
+    };
