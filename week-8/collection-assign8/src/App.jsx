880829cd587a2e75b398ad9e1bda9989ff34846e
Cinthia Koutianov <ckoutianov@mcad.edu>
2026-03-15 19:25:07 -0700

    updated app.css

--- week-8/collection-assign8/src/App.jsx
+++ week-8/collection-assign8/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         "This is a floral, ambery perfume that reinvents freshness with a neroli bud extraction to capture the flower’s fresh scent, sensuality with bio-converted amber to reveal a vibrant warmth, and intensity through a revolutionary musk molecule for an intense yet subtle trail.",
       isLayerable: false,
       image: "./images/prada-paradoxe.jpg",
-      id: "1"
+      id: "1",
     },
 
     {
@@ -28,7 +28,7 @@ function App() {
         "This fragrance features a delightful menu of mouthwatering notes, including fluffy pink marshmallow, succulent strawberry, flaky coconut, delicate freesia, whipped vanilla, and pink musk. Boujee, fluffy, and delicious, this luxurious scent will linger with sweet temptation with every spritz.",
       isLayerable: true,
       image: "./images/kayali-boujee-marsh.jpg",
-      id: "2"
+      id: "2",
     },
 
     {
@@ -40,7 +40,7 @@ function App() {
         "Inspired by Rome, a place where past and present coexist, Born in Roma fragrances tell a story of self-expression: a celebration of people living freely, while embracing their heritage.",
       isLayerable: false,
       image: "./images/valentino-born-in-roma.jpg",
-      id: "3"
+      id: "3",
     },
 
     {
@@ -52,7 +52,7 @@ function App() {
         "An enhanced interpretation of the unexpected floral-fruity fragrance, CHANCE EAU TENDRE Eau de Parfum sweeps you into an intensified whirlwind of tenderness. The signature CHANCE bottle is reinvented with a silver cap and golden accent.",
       isLayerable: false,
       image: "./images/chanel-chance.jpg",
-      id: "4"
+      id: "4",
     },
 
     {
@@ -64,45 +64,59 @@ function App() {
         "Mochi Milk is the newest extension of DedCool’s MILK family of skin scents—fragrances that smell like you, only cozier. Mochi Milk is a sweeter, gourmand take on the warm notes found in both Milk and Xtra Milk. Think sweet rice milk, marshmallows, and vanilla. Words that describe Mochi Milk are sweet, whimsical, and nostalgic.",
       isLayerable: true,
       image: "./images/dedcool-mochi-milk.jpg",
-      id: "5"
+      id: "5",
     },
   ]);
-//returning array without deleted card
+  //returning array without deleted card
   function deleteCard(id) {
-    console.log("delete me :(", id)
+    console.log("delete me :(", id);
     const updatedArray = fragrances.filter((setFragrances) => {
-      return setFragrances.id !==id
-    })
-    setFragrances(updatedArray)
-  }
-  //finding matching card, and copying 
-  function duplicateCard(id) {
-    console.log("duplicate me :)", id)
-    const matchingFragrance = fragrances.find ((setFragrances) => {
-      return setFragrances.id === id
+      return setFragrances.id !== id;
     });
-    const updatedFragrance = {...matchingFragrance, id: nanoid()}
+    setFragrances(updatedArray);
+  }
+  //finding matching card, and copying
+  function duplicateCard(id) {
+    console.log("duplicate me :)", id);
+    const matchingFragrance = fragrances.find((setFragrances) => {
+      return setFragrances.id === id;
+    });
+    const updatedFragrance = { ...matchingFragrance, id: nanoid() };
     setFragrances([...fragrances, updatedFragrance]);
   }
-  
+  // refesh button using DOM
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
   return (
     <>
       <div className="page">
         <Masthead />
-        <p className="item-count">My collection has {fragrances.length} fragrances.</p>
-        <a>INSTRUCTIONS: To delete a fragrance card click 'trash' icon; to duplicate click 'copy' icon.</a>
+        <p className="item-count">
+          My collection has {fragrances.length} fragrances.
+        </p>
+        <a>
+          INSTRUCTIONS: To delete a fragrance card click 'trash' icon; to
+          duplicate click 'copy' icon. 
+        
+        <button className="resetButton" onClick={refreshPage}>
+          Reset Cards 💫
+        </button></a>
 
         <div className="collection">
           {/* a place for my fragrance collection*/}
-          {/* use ItemCard component in loop */} 
-          {fragrances.map((favFragrance => {
+          {/* use ItemCard component in loop */}
+          {fragrances.map((favFragrance) => {
             return (
-              <ItemCard key={favFragrance.id} 
-              deleteFunction={deleteCard}
-              duplicateFunction={duplicateCard}
-              {...favFragrance}/>
-            )
-          }))}
+              <ItemCard
+                key={favFragrance.id}
+                deleteFunction={deleteCard}
+                duplicateFunction={duplicateCard}
+                {...favFragrance}
+              />
+            );
+          })}
         </div>
       </div>
     </>
